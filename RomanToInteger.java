a510714441acd893389ce319ed5c150d796d1b90
Saipraveen1234 <99538681+Saipraveen1234@users.noreply.github.com>
2023-10-14 07:58:33 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int currValue = getRomanValue(c);
+
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+
+            prevValue = currValue;
+        }
+
+        return result;
+    }
+
+    public static int getRomanValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase();
+
+        int integerEquivalent = romanToInt(romanNumeral);
+
+        if (integerEquivalent > 0) {
+            System.out.println("The integer equivalent of " + romanNumeral + " is: " + integerEquivalent);
+        } else {
+            System.out.println("Invalid Roman numeral.");
+        }
+
+        scanner.close();
+    }
+}
